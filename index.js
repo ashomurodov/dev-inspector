ac3c121d2d8b49abe5caa14ebfc4d2377bbfb66c
ashomurodov <ayyuubshomurodov@gmail.com>
2026-03-07 07:51:30 +0500

    chore: remove test comment from index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { init } from './src/core.js'
 
 // Auto-init: just importing this file is enough.
 // Only mounts if NODE_ENV is not 'production'.
-// test
 if (typeof process === 'undefined' || process.env?.NODE_ENV !== 'production') {
   if (typeof window !== 'undefined') {
     if (document.readyState === 'loading') {
