2a3e02707275cc5541e37551c792f99f34a13560
ashomurodov <ayyuubshomurodov@gmail.com>
2026-03-07 08:37:30 +0500

    fix: remove unnecessary console log from index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { init } from './src/core.js'
 
 // Auto-init: just importing this file is enough.
 // Only mounts if NODE_ENV is not 'production'.
-console.log('sssssdsssss')
 if (typeof process === 'undefined' || process.env?.NODE_ENV !== 'production') {
   if (typeof window !== 'undefined') {
     if (document.readyState === 'loading') {
